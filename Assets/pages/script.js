e13c774c4958b522ffa60d55029e7f3e8ab177c1
Robert Grimes <rjgrimes@gmail.com>
2020-09-26 12:34:10 -0500

    Tweaking of javascript for functionality of button comparison to answer value as well as redrawing each question with answer buttons

--- Assets/pages/script.js
+++ Assets/pages/script.js
@@ -5,6 +5,7 @@ var mainDisplay = document.querySelector(".mainDisplay");
 var timer = document.getElementById("Timer");
 var answers = document.getElementById("answer");
 var answerButtons = document.getElementById("buttons");
+var lineBreak = document.getElementById("break");
 var startButton = document.getElementById("start");
 var value = 0;
 
@@ -21,7 +22,7 @@ var quiz = [
         answer2: "Strings",
         answer3: "Numbers",
         answer4: "Other Arrays",
-        correct: 1
+        correct: "1"
     },
     Question2 = {
         question: "Something",
@@ -29,7 +30,7 @@ var quiz = [
         answer2: "right",
         answer3: "wrong",
         answer4: "wrong",
-        correct: 2
+        correct: "2"
     }
 ];
 
@@ -37,44 +38,84 @@ function displayQuestion(index) {
     mainDisplay.textContent = quiz[index].question;
 
     // create 4 buttons
-    var answer1 = document.createElement("button");
-    var answer2 = document.createElement("button");
-    var answer3 = document.createElement("button");
-    var answer4 = document.createElement("button");
-    answer1.setAttribute("class", "button");
-    answer2.setAttribute("class", "button");
-    answer3.setAttribute("class", "button");
-    answer4.setAttribute("class", "button");
+    var answer = document.createElement("button");
+    answer.setAttribute("class", "button");
+    answer.setAttribute("id", "1");
+    answer.textContent = quiz[index].answer1;
+    answerButtons.appendChild(answer);
 
-    // append created elements
-    answerButtons.appendChild(answer1);
-    answerButtons.appendChild(answer2);
-    answerButtons.appendChild(answer3);
-    answerButtons.appendChild(answer4);
+    answer = document.createElement("button");
+    answer.setAttribute("class", "button");
+    answer.setAttribute("id", "2");
+    answer.textContent = quiz[index].answer2;
+    answerButtons.appendChild(answer);
 
-    // set button text
-    answer1.textContent = quiz[index].answer1;
-    answer2.textContent = quiz[index].answer2;
-    answer3.textContent = quiz[index].answer3;
-    answer4.textContent = quiz[index].answer4;
+    answer = document.createElement("button");
+    answer.setAttribute("class", "button");
+    answer.setAttribute("id", "3");
+    answer.textContent = quiz[index].answer3;
+    answerButtons.appendChild(answer);
 
-    answer1.addEventListener("click", determineAnswer(answer, index));
-    answer2.addEventListener("click", determineAnswer(answer, index));
-    answer3.addEventListener("click", determineAnswer(answer, index));
-    answer4.addEventListener("click", determineAnswer(answer, index));
+    answer = document.createElement("button");
+    answer.setAttribute("class", "button");
+    answer.setAttribute("id", "4");
+    answer.textContent = quiz[index].answer4;
+    answerButtons.appendChild(answer);
 };
 
-function determineAnswer(answer, index){
+// function used to redraw all quiz items as well as update quizIndex
+// will also delete previously created buttons
+function redrawQuiz(quizIndex) {
+    if (quizIndex == quiz.length) {
+        alert("Quiz finished.");
+    }
+    else {
+        quizIndex++;
+        answerButtons.innerHTML = '';
+        displayQuestion(quizIndex);
+        answerChecking(quizIndex);
+    }
+};
 
+function answerChecking(quizIndex) {
+    var allAnswers = document.querySelectorAll("button");
+    // console.log("Found " + allAnswers.length + " buttons.");
+    for (var i = 0; i < allAnswers.length; i++) {
+        allAnswers[i].addEventListener("click", function () {
+            // testing to determine which button was clicked
+            console.clear();
+            console.log("Correct answer: " + quiz[quizIndex].correct);
+            console.log("You clicked: ", this.innerHTML);
+            console.log(this.id);
+            if (this.id === quiz[quizIndex].correct) {
+                // console.log("Correct!");
+                redrawQuiz(quizIndex);
+                lineBreak.setAttribute("class", "visible font-weight-light text-break");
+                answers.setAttribute("class", "visible font-weight-light font-italic");
+                answers.textContent = "Correct!";
+            }
+            else {
+                // console.log("Wrong");
+                timeLeft -= 10;
+                redrawQuiz(quizIndex);
+                lineBreak.setAttribute("class", "visible font-weight-light text-break");
+                answers.setAttribute("class", "visible font-weight-light font-italic");
+                answers.textContent = "Wrong!";
+            }
+        })
+    }
 
 };
 
 function startQuiz() {
-    startButton.style.display = "none";
+    startButton.parentNode.removeChild(startButton);
     // target timer with count variable
     timer.textContent = timeLeft;
 
+    // display first question
     displayQuestion(quizIndex);
+    answerChecking(quizIndex);
+
 
     var timerInterval = setInterval(function () {
         // seconds--;
@@ -88,9 +129,8 @@ function startQuiz() {
 
         // if seconds = 0
         if (timeLeft == 0) {
-            // clear function
-            // disable answers if any are displayed
-            // display high score submission block
+            clearInterval(timerInterval);
+            // alert("Time ran out. Quiz over.");
         }
     }, 1000);
 }
