be338a7c70a59c0ed922ce36bf85c67af9b457fc
Robert Grimes <rjgrimes@gmail.com>
2020-09-29 18:28:46 -0500

    Finalized scripting for highscores page

--- Assets/pages/scoreScript.js
+++ Assets/pages/scoreScript.js
@@ -1,24 +1,42 @@
 //#region VariableDeclaration
-var home = document.getElementById("mainPaige");
 var back = document.getElementById("back");
 var clear = document.getElementById("clear");
+var listDisplay = document.querySelector(".ScoreDisplay");
+
+var userScoreLocal = JSON.parse(localStorage.getItem("userScoreLocal")) || [];
+// console.log(userScoreLocal);
+// console.log(userScoreLocal.length);
 //#endregion
 
 // Function to go back to main index page
-function goHome(){
-    window.location.href="index.html"
+function goHome() {
+    window.location.href = "../../index.html"
 }
 
 // Function to clear local storage of all scores
-function clearHS(){
+function clearHS() {
     localStorage.clear();
     // empty generated list after clearing local storage
+    displayScores();
 }
 
+// function to display all high scores on page
+window.onload = function displayScores() {
+    for (var i = 0; i < userScoreLocal.length; i++) {
+        var listItem = document.createElement("li");
+        // userScore.name = userScoreLocal[i].name;
+        // console.log(userScore.name);
+        // userScore.score = userScoreLocal[i].score;
+        // console.log(userScore.score);
+        listItem.textContent = userScoreLocal[i].name + ": " + userScoreLocal[i].score;
+        // listItem.textContent = userScore.name + " - " + userScore.score;
+        listDisplay.appendChild(listItem);
+    }
+}
 
 
 
 
 // event listeners
-home.addEventListener("click", goHome);
 back.addEventListener("click", goHome);
+clear.addEventListener("click", clearHS);
